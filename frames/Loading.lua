df4e12ff790c1e96af0892e8e5c17227b6fe5783
Sleve-m <jacekellyofficial@gmail.com>
2025-12-17 12:13:37 -0500

    Update Loading.lua

--- frames/Loading.lua
+++ frames/Loading.lua
@@ -3,8 +3,8 @@ local Loading = {}
 function Loading.createLoadingUI()
     local loadingui = Instance.new("TextLabel")
     loadingui.Text = "NOCT xt"
-    loadingui.Size = UDim2.new(0,240,0,70)
-    loadingui.Position = UDim2.new(0.5,-120,0.5,-35)
+    loadingui.Size = UDim2.new(0,260,0,80)
+    loadingui.Position = UDim2.new(0.5,-230,0.5,-40)
     loadingui.BorderSizePixel = 0
     loadingui.BackgroundColor3 = Color3.new(0,0,0)
     loadingui.TextColor3 = Color3.fromRGB(200,200,200)
